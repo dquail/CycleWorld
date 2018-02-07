82373c20cd67aaca9f9ba674340c0c068af2cbf3
David Quail <quail.david@gmail.com>
2018-02-07 09:54:33 -0800

    Added DoubleQ.py but did not integrate it into learning foreground

---
+++ src/DoubleQ.py
@@ -0,0 +1,62 @@
+from pylab import *
+import numpy
+
+
+
+
+class DoubleQ:
+    def __init__(self, alpha, eps, numberOfFeatures):
+        self.numberOfFeatures = numberOfFeatures
+        actionOffset = numberOfFeatures
+        self.numberOfWeights = numberOfFeatures * 2
+        self.alpha = alpha
+        self.eps = eps
+
+        self.theta1 = -0.001 * rand(self.numberOfWeights)
+        self.theta2 = -0.001 * rand(self.numberOfWeights)
+
+    def resetQ(self):
+        self.theta1 = -0.001 * rand(self.numberOfWeights)
+        self.theta2 = -0.001 * rand(self.numberOfWeights)
+
+    def qHat(self, state, action, theta):
+        value = 0
+        for indicie in state:
+            value += theta[indicie + action * actionOffset]
+        return value
+
+    def bestAction(self, state, theta):
+        return numpy.argmax([self.qHat(state, 0, theta), self.qHat(state, 1, theta), self.qHat(state, 2, theta)])
+
+    def policy(self, state):
+        # Combine the average of the two weight vectors
+        theta = np.add(self.theta1, self.theta2) / 2
+        return numpy.argmax([self.qHat(state, 0, theta), self.qHat(state, 1, theta), self.qHat(state, 2, theta)])
+
+    def learn(self, state, action, nextState, reward):
+        # determine which theta to update
+        newStateValue = 0
+        # tileIndices = state
+
+        if (random() > 0.5):
+            # print("Using theta1")
+            nextStateValue = 0
+            if (nextState):
+                # Non terminal
+                theta1BestNextAction = self.bestAction(nextState, self.theta1)
+                newStateValue = self.qHat(nextState, theta1BestNextAction, self.theta2)
+
+            learningError = self.alpha * (reward + newStateValue - self.qHat(state, action, self.theta1))
+            for indicie in state:
+                self.theta1[indicie + action * actionOffset] += learningError
+        else:
+            # print("Using theta2")
+            nextStateValue = 0
+            if (nextState):
+                # Non terminal
+                theta2BestNextAction = self.bestAction(nextState, self.theta2)
+                newStateValue = self.qHat(nextState, theta2BestNextAction, self.theta1)
+
+            learningError = self.alpha * (reward + newStateValue - self.qHat(state, action, self.theta2))
+            for indicie in state:
+                self.theta2[indicie + action * actionOffset] += learningError
